e9dd5450de87de6ef69226b20464012681d806f2
philippeb8 <philippeb8@gmail.com>
2016-03-18 19:29:18 -0400

    Updated license info

--- example/t100_test1.cpp
+++ example/t100_test1.cpp
@@ -3,7 +3,7 @@
 	t100_test1.cpp
 
 	@note
-	Copyright (c) 2008 Phil Bouchard <phil@fornux.com>.
+	Copyright (c) 2008-2016 Phil Bouchard <pbouchard8@gmail.com>.
 
 	Distributed under the Boost Software License, Version 1.0.
 
