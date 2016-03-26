b1e56af2180ee5606b71f5f52b68b0aa591c1fe4
philippeb8 <philippeb8@gmail.com>
2016-03-26 17:29:50 -0400

    Adding placement delete

--- include/boost/smart_ptr/detail/node_base.hpp
+++ include/boost/smart_ptr/detail/node_base.hpp
@@ -360,6 +360,19 @@ template <typename T, typename PoolAllocator = pool_allocator<T> >
             static_cast<node *>(p)->a_.deallocate(static_cast<node *>(p), 1);
         }
 
+
+        /**
+            Deallocates a @c node from @c PoolAllocator .
+
+            @param  p   Address of the @c node to deallocate.
+            @param  a   Copy of @c PoolAllocator to be used.
+        */
+
+        void operator delete (void * p, allocator_type a)
+        {
+            a.deallocate(static_cast<node *>(p), 1);
+        }
+
     private:
         using node_element<T>::elem_;
         
