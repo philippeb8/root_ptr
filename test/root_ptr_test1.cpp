ce3a350cd37bbd253295f5ef2646900b3be16156
philippeb8 <philippeb8@gmail.com>
2016-03-30 23:12:47 -0400

    Syncing tests

--- test/root_ptr_test1.cpp
+++ test/root_ptr_test1.cpp
@@ -1,23 +1,23 @@
 /**
-	@file
-	block_ptr_test2.cpp
+    @file
+    root_ptr_test2.cpp
 
-	@note
-	Copyright (c) 2008 Phil Bouchard <phil@fornux.com>.
+    @note
+    Copyright (c) 2008-2016 Phil Bouchard <pbouchard8@gmail.com>.
 
-	Distributed under the Boost Software License, Version 1.0.
+    Distributed under the Boost Software License, Version 1.0.
 
-	See accompanying file LICENSE_1_0.txt or copy at
-	http://www.boost.org/LICENSE_1_0.txt
+    See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt
 
-	See http://www.boost.org/libs/smart_ptr/doc/index.html for documentation.
+    See http://www.boost.org/libs/smart_ptr/doc/index.html for documentation.
 */
 
 #include <list>
 #include <iostream>
 #include <algorithm>
 
-#include <boost/block_ptr.hpp>
+#include <boost/smart_ptr/root_ptr.hpp>
 #include <boost/current_function.hpp>
 
 #define BOOST_TEST_DYN_LINK
@@ -32,59 +32,61 @@ using namespace boost;
 
 struct A
 {
-	static int counter;
-	static list<int> order;
-	
-	int i;
-	block_ptr<A> p;
-	
-	A() : i(counter ++) 
-	{
-	}
-	
-	~A()
-	{
-		order.push_back(i);
-	}
+    static int counter;
+    static list<int> order;
+    
+    int i;
+    node_ptr<A> p;
+    
+    A(node_proxy const & x) : i(counter ++) , p(x)
+    {
+    }
+    
+    ~A()
+    {
+        order.push_back(i);
+    }
 };
 
 int A::counter = 0;
 list<int> A::order;
 
-BOOST_AUTO_TEST_CASE(block_ptr_test1)
+BOOST_AUTO_TEST_CASE(root_ptr_test1)
 {
-	{
-		block_ptr<A> p = make_block<A>(); // 0
-		block_ptr<A> q = make_block<A>(); // 1
-		block_ptr<A> r = make_block<A>(); // 2
+    {
+        root_ptr<A> x;
+        node_ptr<A> p = make_node<A>(x, x); // 0
+        node_ptr<A> q = make_node<A>(x, x); // 1
+        node_ptr<A> r = make_node<A>(x, x); // 2
 
-		block_ptr<void> t = make_block<A>(); // 3
+        root_ptr<A> t = make_root<A>(x); // 3
 
-		p->p = p;
-		q = r;
+        p->p = p;
+        q = r;
 
-	}
-	int order1[] = {1, 3, 2, 0};
-	BOOST_CHECK( equal(A::order.begin(), A::order.end(), order1) );
-	A::order.clear();
+    }
+    int order1[] = {1, 3, 2, 0};
+    BOOST_CHECK( equal(A::order.begin(), A::order.end(), order1) );
+    A::order.clear();
 
-	// The following don't work with MSVC:
-#if ! defined(_MSC_VER)
-	{
-		block_ptr<A[5]> s = make_block<A[5]>(); // 4, 5, 6, 7, 8
-	}
-	int order2[] = {8, 7, 6, 5, 4};
-	BOOST_CHECK( equal(A::order.begin(), A::order.end(), order2) );
-	A::order.clear();
+    // The following don't work with MSVC:
+#if 0 //! defined(_MSC_VER)
+    {
+        root_ptr<A[5]> s = make_root<A[5]>(); // 4, 5, 6, 7, 8
+    }
+    int order2[] = {8, 7, 6, 5, 4};
+    BOOST_CHECK( equal(A::order.begin(), A::order.end(), order2) );
+    A::order.clear();
 #endif
 
-	{
-		block_ptr<A> v = make_block<A>(); // 9
-		v->p = make_block<A>(); // 10
-		v->p->p = make_block<A>(); // 11
-		v->p->p->p = v;
-	}
-	int order3[] = {9, 10, 11};
-	BOOST_CHECK( equal(A::order.begin(), A::order.end(), order3) );
-	A::order.clear();
+    {
+        root_ptr<A> x;
+        node_ptr<A> v = make_node<A>(x, x); // 4
+        v->p = make_node<A>(x, x); // 5
+        v->p->p = make_node<A>(x, x); // 6
+        v->p->p->p = v;
+    }
+    int order3[] = {4, 5, 6};
+    BOOST_CHECK( equal(A::order.begin(), A::order.end(), order3) );
+    A::order.clear();
 }
--- test/root_ptr_test3.cpp
+++ test/root_ptr_test3.cpp
@@ -1,6 +1,6 @@
 /**
 	@file
-	block_ptr_test3.cpp
+	root_ptr_test3.cpp
 
 	@note
 	Copyright (c) 2008 Steven Watanabe <watanabesj@gmail.com>
@@ -15,8 +15,7 @@
 
 
 
-#include <boost/block_ptr.hpp>
-#include <boost/block_allocator.hpp>
+#include <boost/smart_ptr/root_ptr.hpp>
 
 #include <vector>
 #include <iostream>
@@ -34,60 +33,62 @@ static int count;
 
 using namespace boost;
 
-struct node {
-    node() {
+struct list_node {
+    list_node(node_proxy const & x) : prior(x), next(x) {
         ++count;
     }
-    ~node() {
+    ~list_node() {
         --count;
     }
-    node(const node&) {
-        ++count;
-    }
-    block_ptr<node> prior;
-    block_ptr<node> next;
+    node_ptr<list_node> prior;
+    node_ptr<list_node> next;
 };
 
 struct list {
 public:
     list() {}
     void clear() {
-        front.reset();
-        back.reset();
+        root.reset();
     }
     void insert() {
-        if(front.get() == 0) {
-            front = back = make_block<node>();
+        if(root.get() == 0) {
+            root = new node<list_node>(root);
         } else {
-            back->next = make_block<node>();
-            back->next->prior = back;
-            back = back->next;
+            root->next = new node<list_node>(root);
+            root->next->prior = root;
+            root = root->next;
         }
     }
+    ~list()
+    {
+    }
 private:
-    block_ptr<node> front;
-    block_ptr<node> back;
+    root_ptr<list_node> root;
 };
 
 struct vector {
     vector() { ++count; }
     ~vector() { --count; }
     vector(const vector& other) : elements(other.elements) { ++count; }
-    std::vector<block_ptr<vector>, block_allocator<block_ptr<vector> > > elements;
+    std::vector<root_ptr<vector> > elements;
 };
 
 struct create_type {
+    create_type(node_proxy & x) : x_(x) {}
+    
     template<class T>
     void operator()(T) const {
-        make_block<boost::array<char, T::value> >();
+        node_ptr<boost::array<char, T::value> >(x_, new node<boost::array<char, T::value> >());
     }
+    
+    node_proxy & x_;
 };
 
-BOOST_AUTO_TEST_CASE(test_block_ptr) {
+BOOST_AUTO_TEST_CASE(test_root_ptr) {
 
     count = 0;
     {
-        block_ptr<vector> v = make_block<vector>();
+        root_ptr<vector> v = make_root<vector>();
         v->elements.push_back(v);
     }
     BOOST_CHECK_EQUAL(count, 0);
@@ -106,27 +107,32 @@ BOOST_AUTO_TEST_CASE(test_block_ptr) {
 
     count = 0;
     {
-        block_ptr<int> test = make_block<int>(5);
+        root_ptr<int> test = make_root<int>(5);
         test = test;
         
         BOOST_CHECK_NE(test.get(), static_cast<int*>(0));
         BOOST_CHECK_EQUAL(*test, 5);
     }
 
-    for(int i = 0; i < 500; ++i) {
-        boost::mpl::for_each<boost::mpl::range_c<int, 1, 100> >(create_type());
+    count = 0;
+    {
+        root_ptr<int> x;
+        for(int i = 0; i < 500; ++i) {
+            boost::mpl::for_each<boost::mpl::range_c<int, 1, 100> >(create_type(x));
+        }
     }
+    BOOST_CHECK_EQUAL(count, 0);
 
     count = 0;
     {
-        block_ptr<vector> v = make_block<vector>();
+        root_ptr<vector> v = make_root<vector>();
         v->elements.push_back(v);
     }
     BOOST_CHECK_EQUAL(count, 0);
 
     {
         vector v;
-        v.elements.push_back(make_block<vector>());
+        v.elements.push_back(make_root<vector>());
     }
     BOOST_CHECK_EQUAL(count, 0);
 
