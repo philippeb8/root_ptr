7a57be36d9a3a9a1ad97800d72a002441695b260
philippeb8 <philippeb8@gmail.com>
2017-07-16 22:50:23 -0400

    Adding distinct X++ example

--- xpp2cpp/tests/input1.js
+++ xpp2cpp/tests/input1.js
@@ -1,20 +1,17 @@
 {
-    var temporary = new int(0);
+    var temporary = 0;
     
     var bar = function ()
     {
-        return new int(10);
+        return 10;
     };
     
     var foo = function ()
     {
-        var object = new int(11);
+        var object = 11;
         return function(argument) { return argument; };
     };
     
-    for (int i = 0; i < 1000000; ++ i)
-        cout << (foo())(temporary) << endl;
-
-    //for (var i = 0; i < 1000000; ++ i)
-    //    console.log((foo())(temporary));
+    for (var i = 0; i < 1000000; ++ i)
+        console.log((foo())(temporary));
 }
